b5d6bdb77af40f1236c51b34ea08ea00ce51c1df
khanhnx2 <nguyenkhanhvietnam@gmail.com>
2024-07-13 20:48:58 +0700

    cm

--- ktp/settings.py
+++ ktp/settings.py
@@ -127,22 +127,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(STATIC_ROOT, 'ktpdebug.txt'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
